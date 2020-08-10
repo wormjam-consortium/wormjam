805ab3856cd7fef359cf0a873105f100ddca4aab
Jake Hattwell <j.hattwell@uq.edu.au>
2020-08-10 13:55:58 +1000

    WormJam v0.1.0 - Prerelease before curation round #3 (#48)
    * Relocated Website/Documentation to WormJam Consortium Organisation
    
    * Added Requirements.txt
    
    * Improved annotations
    
    * Added continuous integration pipeline, targeted at the devel branch that builds, tests and reports on the model
    
    Co-authored-by: Michael Witting <michael.witting@helmholtz-muenchen.de>

--- travis/send_reports.py
+++ travis/send_reports.py
@@ -5,20 +5,21 @@ import requests
 import datetime
 
 DISCORD_ENDPOINT = sys.argv[1]
-TRAVIS_BUILD_NUMBER = sys.argv[2]
-TRAVIS_BUILD_WEB_URL = sys.argv[3]
-DISCORD_ENDPOINT_2 = sys.argv[4]
+DISCORD_ENDPOINT_2 = sys.argv[2]
+TRAVIS_BUILD_NUMBER = sys.argv[3]
+TRAVIS_BUILD_WEB_URL = sys.argv[4]
+TRAVIS_REPO_SLUG = sys.argv[5]
 
 timestamp = datetime.datetime.now().strftime("%Y_%m_%d__%H_%M_%S")
 filename = "WormJam"+timestamp+".tar.gz"
 
-files = {'results.json': open('results.json', 'rb')}
+files = {'Report.html': open('Report.html', 'rb')}
 files2 = {filename:open("WormJam.tar.gz",'rb')}
 payload_json = {
     "embeds": [{
         "title": "WormJam CI Report",
         "color": 16709211,
-        "description": "PR Merge",
+        "description": "Model Build from [%s](%s)"%(TRAVIS_REPO_SLUG,"https://github.com/"+TRAVIS_REPO_SLUG),
         "fields":[
             {
                 "name": "Build Number",
